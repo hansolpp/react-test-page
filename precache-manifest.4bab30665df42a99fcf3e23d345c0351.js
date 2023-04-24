52871efc6b2dadfb32c5a98b7bd17a9f92976c42
clay <hansolmin3@gmail.com>
2023-04-24 23:41:45 +0900

    Updates

---
+++ precache-manifest.4bab30665df42a99fcf3e23d345c0351.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "615bbecd664cb1b864a179c947ace359",
+    "url": "/react-test-page/index.html"
+  },
+  {
+    "revision": "e14930c25d3c7067fd77",
+    "url": "/react-test-page/static/css/main.550956d2.chunk.css"
+  },
+  {
+    "revision": "4389ee9112b35282ccbb",
+    "url": "/react-test-page/static/js/2.d885b25a.chunk.js"
+  },
+  {
+    "revision": "94ba14b155f70c1aa13170e885f70a4c",
+    "url": "/react-test-page/static/js/2.d885b25a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e14930c25d3c7067fd77",
+    "url": "/react-test-page/static/js/main.9ec2ca7b.chunk.js"
+  },
+  {
+    "revision": "018d4bb3ed6dae9b90bc",
+    "url": "/react-test-page/static/js/runtime-main.9687ab72.js"
+  }
+]);
